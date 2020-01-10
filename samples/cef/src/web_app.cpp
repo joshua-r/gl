9203e3750e4cda64046dfd4ed5cd8faba3ef692a
Joshua Reibert <joshua.reibert@dlr.de>
2020-01-10 14:21:35 +0100

    Remove unused webapp

--- samples/cef/src/web_app.cpp
+++
@@ -1,9 +0,0 @@
-#include <web_app.hpp>
-
-WebApp::WebApp() {}
-
-CefRefPtr<CefBrowserProcessHandler> WebApp::GetBrowserProcessHandler() {
-  return this;
-}
-
-void WebApp::OnContextInitialized() {}
